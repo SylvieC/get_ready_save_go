61a2fb770454b29e1681b24cb5feee5252143976
SylvieC <sylviecottrell@gmail.com>
2014-03-21 13:18:23 -0700

    map with start and end working

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,7 +2,7 @@ class TripsController < ApplicationController
   before_filter :authenticate_user!
   def index
     @trip = Trip.new
-    @trips = Trip.all |
+    @trips = Trip.all 
   end
 
   def new
@@ -13,9 +13,7 @@ class TripsController < ApplicationController
     @trip = Trip.new(trip)
     @trip.user_id = current_user.id
     @trip.save
-    redirect_to  user_trip_path(current_user.id)
+    redirect_to  user_path(current_user.id)
   end
 
-  def update
-  end
- end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    trip = current_user.trips.last
+    gon.beg = trip.from_city || "san francisco"
+    gon.finish = trip.to_city || "paris, France"
   end
 end
