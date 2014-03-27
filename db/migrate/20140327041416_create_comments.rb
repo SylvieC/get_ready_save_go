98462b192ead931de9c8c213327b4bd0ca642429
SylvieC <sylviecottrell@gmail.com>
2014-03-27 09:19:21 -0700

    deleted one migration file create comments

--- db/migrate/20140327041416_create_comments.rb
+++
@@ -1,8 +0,0 @@
-class CreateComments < ActiveRecord::Migration
-  def change
-    create_table :comments do |t|
-      t.text :content
-      t.belongs_to :activity, index: true
-    end
-  end
-end
