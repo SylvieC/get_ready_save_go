460c6ebe29d88b8bd1fe8ac2cacf59f1bf4053ed
SylvieC <sylviecottrell@gmail.com>
2014-06-21 13:45:32 -0700

    made suggestions appear after first user enters a trip

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -1,15 +1,16 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :trip do
-    from_city "MyString"
-    from_country "MyString"
-    to_city "MyString"
-    to_country "MyString"
-    money_saved 1.5
-    add_to_money_saved 1.5
-    cost 1.5
-    days_before_departure 1
-    months_before_departure 1
-  end
+  factory :trip do |f|
+    user
+    f.from_city "San Francisco, USA"
+    f.to_city "Paris, France"
+    f.cost  1200
+    f.start_date "2014-05-23 00:00:00"
+    f.title  "La France"
+    f.user_id 10
 end
+   
+end
+
+
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Trip do
-  pending "add some examples to (or delete) #{__FILE__}"
+ it "is is invalid if from_city is nil" do
+  Trip = Trip.new(
+   from_city: "San Francisco, USA",
+   to_city: "Paris, France",
+   cost:  1200,
+   start_date: "2014-05-23 00:00:00",
+   title:  "La France",
+   user_id: 10
+
+    )
+ end
 end
